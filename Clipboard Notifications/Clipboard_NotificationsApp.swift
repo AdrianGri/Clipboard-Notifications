e025e7d455e1094346c34bd90677c35fe93c5f68
Adrian Gri <adrian.gri03@gmail.com>
2024-09-25 15:28:22 +0200

    Initial Commit

---
+++ Clipboard Notifications/Clipboard_NotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clipboard_NotificationsApp.swift
+//  Clipboard Notifications
+//
+//  Created by Adrian Gri on 2024-09-25.
+//
+
+import SwiftUI
+
+@main
+struct Clipboard_NotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clipboard NotificationsTests/Clipboard_NotificationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Clipboard_NotificationsTests.swift
+//  Clipboard NotificationsTests
+//
+//  Created by Adrian Gri on 2024-09-25.
+//
+
+import Testing
+@testable import Clipboard_Notifications
+
+struct Clipboard_NotificationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
